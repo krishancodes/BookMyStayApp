0ebe2e52a5ccafe756509bfdb2c554ca39b0e737
krishancodes <kk5583@srmist.edu.in>
2026-04-01 14:42:18 +0530

    Initial project setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay Application
+ * Entry point of the Hotel Booking Management System
+ * Demonstrates application startup behavior.
+ *
+ * @author Krishan
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    // UC1: Application Entry & Welcome Message
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Welcome to Book My Stay    ");
+        System.out.println("      Hotel Booking System       ");
+        System.out.println("      Version: 1.0               ");
+        System.out.println("=================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay.");
+    }
+}
